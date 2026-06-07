a584246ab37c2c92d94445bde50b9890dd16cff0
sahil-iftekhar <mailsahiliftekhar@gmail.com>
2026-06-08 03:58:26 +0600

    Navbar and global buttons component created

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Welcome to SESSION-AUTH</h1>
-      <p>
+    <div className="max-w-4xl mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-6">Welcome to SESSION-AUTH</h1>
+      
+      <p className="mb-4 text-gray-700 leading-relaxed font-['Merriweather'] font-bold text-[20px] leading-[25px] text-left text-black">
         A fullstack web application built with <strong>NextJS</strong> for the
         frontend and <strong>Django</strong> for the backend, utilizing{" "}
         <strong>REST</strong> APIs and a <strong>Postgres</strong> database.
@@ -19,16 +22,21 @@ export default function Page() {
         social providers if the account is created using social account.
         Otherwise a default image is set for password users.
       </p>
-      <p>
+
+      <p className="mb-4 text-gray-700 leading-relaxed font-['Merriweather'] font-bold text-[20px] leading-[25px] text-left text-black">
         <strong>Superusers</strong> and <strong>Admins</strong> have elevated
         priviliges where they can access the <strong>Admin Dashboard</strong>{" "}
-        and can activate, deactivate, edit or delete an user according to thier
+        and can activate, deactivate, edit or delete an user according to their
         priviliges.
       </p>
-      <p>
+
+      <p className="mb-8 text-gray-700 font-['Merriweather'] font-bold text-[20px] leading-[25px] text-center text-black">
         For more details, visit the{" "}
         <a
           href="https://github.com/codextraa/SESSION_AUTH_django_rest"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
         >
           GitHub repository
         </a>
--- frontend/src/libs/api.js
+++ frontend/src/libs/api.js
@@ -2,7 +2,7 @@ import { ApiClient } from "./apiClient";
 
 const HTTPS = process.env.HTTPS === "true";
 const API_URL = HTTPS
-  ? process.env.API_BASE_HTTPS_URL
+  ? process.env.API_BASE_HTTPS_OLD_URL
   : process.env.API_BASE_URL;
 const apiClient = new ApiClient(API_URL);
 
