99bbba15d65589541ba4842219e4113ce986aa8d
arianasuha <arianahoque94@gmail.com>
2026-06-23 21:19:33 +0600

    Created 2FA serializer, verify otp and 2FA throttling system

--- server/auth_api/request_serializers.py
+++ server/auth_api/request_serializers.py
@@ -48,3 +48,30 @@ class LoginRequestSerializer(BaseRecaptchaSerializer):  # pylint: disable=W0223
             "null": "Password is required.",
         },
     )
+
+
+class TwoFARequestSerializer(serializers.Serializer):  # pylint: disable=W0223
+    """
+    Handles 2FA credentials.
+    """
+    pre_auth_token = serializers.CharField(
+        required=True,
+        allow_null=False,
+        allow_blank=False,
+        help_text="The raw pre-auth token to be used in subsequent requests.",
+        error_messages={
+            "required": "Token is required.",
+            "blank": "Token is required.",
+            "null": "Token is required.",
+        },
+    )
+    otp = serializers.IntegerField(
+        required=True,
+        allow_null=False,
+        help_text="The One Time Password to be used in subsequent requests.",
+        error_messages={
+            "required": "OTP is required.",
+            "null": "OTP is required.",
+            "invalid": "OTP is invalid.",
+        },
+    )
